0b0a8a1df98d0a6f6c1fc3a5c290e1ae9760752c
Rafael Bravo <guinea83@gmail.com>
2018-11-07 23:00:15 +0000

    make index.php redirect to the yii application

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php
-    phpinfo();
+  header('HTTP/1.1 307 Temporary Redirect');
+header('Location:app/web');
+?>
+<html>
+  <head>
+    <title>Moved</title>
+  </head>
+  <body>
+    <h1>Moved</h1>
+    <p>This page has moved to <a href="app/web">mi app</a>.</p>
+  </body>
+</html>
 ?>
